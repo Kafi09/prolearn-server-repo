fc6358b580cfc89886abb8da9bf1c5b6673d1c20
Kafi09 <kaficsmy9@gmail.com>
2022-10-27 16:12:39 +0600

    added learn.json

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const courses = require('./data/courses.json');
+const course = require('./data/learn.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+app.get('/courses-category', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '06') {
+        res.send(course);
+    }
+    else {
+        const category_course = course.filter(n => n.category_id === id);
+        res.send(category_course);
+    }
 })
+
+app.get('/course', (req, res) => {
+    res.send(course);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
     console.log('server running', port);
     // console.log(`Example app listening on port ${port}`)
-})
+});
